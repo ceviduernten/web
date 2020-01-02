1149723b1051394d6b457c8658a8ec746ab5a794
Urs Forrer <mail@ursforrer.ch>
2020-01-02 17:46:43 +0100

    Final version with is deployed to the webpage.

--- chaestli.js
+++ chaestli.js
@@ -1,26 +1,24 @@
-$(function(){
-    function renderChaestlizettel(group) {
-        // code to be executed
-        $("#chaestli-nodata").hide();
-        $("#chaestli-error").hide();
-        $("#chaestli-result").hide();
-        let url = "https://api.ceviduernten.ch/Appointments/" + group + "/next";
-        $.get(url, function(data, status){
-            if (status === "success" && data.statusCode === 200) {
-                let result = data.data;
-                if (result == null) {
-                    $("#chaestli-nodata").show();
-                } else {
-                    let bla = new Date(result.date).toLocaleDateString();
-                    let timeString = bla + ", " + result.time;
-                    $("#data-time").text(timeString);
-                    $("#data-location").text(result.location);
-                    $("#data-infos").text(result.infos);
-                    $("#chaestli-result").show();
-                }
+var renderChaestlizettel = function(group) {
+    // code to be executed
+    $("#chaestli-nodata").hide();
+    $("#chaestli-error").hide();
+    $("#chaestli-result").hide();
+    let url = "https://api.ceviduernten.ch/Appointments/" + group + "/next";
+    $.get(url, function(data, status){
+        if (status === "success" && data.statusCode === 200) {
+            let result = data.data;
+            if (result == null) {
+                $("#chaestli-nodata").show();
             } else {
-                $("#chaestli-error").show();
+                let bla = new Date(result.date).toLocaleDateString();
+                let timeString = bla + ", " + result.time;
+                $("#data-time").text(timeString);
+                $("#data-location").text(result.location);
+                $("#data-infos").text(result.infos);
+                $("#chaestli-result").show();
             }
-        });
-    }
-});
+        } else {
+            $("#chaestli-error").show();
+        }
+    });
+}
